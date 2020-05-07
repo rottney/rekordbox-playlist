930e292bb18c723a94444d12421afbb7104dad7a
Ryan Ottney <rottney123@gmail.com>
2020-05-07 10:50:34 -0700

    cleaned up formatting

--- RekordboxPlaylist.php
+++ RekordboxPlaylist.php
@@ -8,8 +8,8 @@
 	$track_list = $xml -> xpath("PLAYLISTS/NODE/NODE[@Name = '" . $playlist_name . "']/TRACK");
 
 	while ($num_tracks == 0) {
-		echo "Playlist '" . $playlist_name . "' is not a valid playlist, or it contains no tracks.\n";
-		echo "Please enter a valid playlist name.\n";
+		echo "Playlist '" . $playlist_name . "' is not a valid playlist, or it contains no tracks.\n"
+			. "Please enter a valid playlist name.\n";
 		$playlist_name = trim(fgets(STDIN));
 		$num_tracks = ($xml -> xpath("PLAYLISTS/NODE/NODE[@Name = '" . $playlist_name . "']/@Entries"))[0];
 		$track_list = $xml -> xpath("PLAYLISTS/NODE/NODE[@Name = '" . $playlist_name . "']/TRACK");
@@ -23,7 +23,8 @@
 		$label = $xml -> xpath("COLLECTION/TRACK[@TrackID = '" . $key . "']/@Label")[0];
 		fwrite($myfile, $i + 1 . ") " . $artist . " - " . $track_name . " [" . $label . "]\n");
 	}
+	fclose($myfile);
+
 	echo "Playlist has been exported to file " . $playlist_name . ".txt,\n"
 		. "which is located in the same folder as this script.\n";
-	fclose($myfile);
 ?>
